64015725683192c974fb9330a2e26e6dd7482c46
tribagus <tri.bagus21@gmail.com>
2022-09-10 10:46:00 +0700

    add barchart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import { useState, useEffect } from "react"
 import "./App.css"
+import BarChart from "./components/BarChart"
+import { orderList } from "./data"
 
 function App() {
-  return <div className="App"></div>
+  const [chart, setChart] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: "",
+        data: [],
+      },
+    ],
+  })
+
+  useEffect(() => {
+    setChart({
+      labels: orderList.map((data) => data.date),
+      datasets: [
+        {
+          label: "data penjualan",
+          data: orderList.map((data) => data.totalOrder),
+        },
+      ],
+    })
+  }, [])
+
+  // console.log(chart)
+
+  return (
+    <div className="App">
+      <BarChart chart={chart} />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/BarChart.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Bar } from "react-chartjs-2"
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js"
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+const BarChart = ({ chart }) => {
+  return <Bar data={chart} />
+}
+
+export default BarChart
